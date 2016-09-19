6e58c3897d28a0f11048ae23401edf3174d7cb34
Christian Wünsch <23318276+cwuensch@users.noreply.github.com>
2016-09-19 15:11:53 +0000

    HumaxHeader.h in C übersetzt

--- HumaxHeader.h
+++ HumaxHeader.h
@@ -24,79 +24,72 @@
    (d) THumaxBlock_Ende           -> in diesem Teil stehen keine relevanten Informationen mehr
 */
 
+#include "type.h"
 
-Private Const HumaxHeaderIntervall = &H8000&  ' nach diesem Intervall wird der Header wiederholt
-Private Const HumaxHeaderLaenge = &H4A0       ' die Länge eines jeden Headers
-Private Const HumaxHeaderAnfang = &HFD04417F  ' das erste DWORD eines jeden Headers
-Private Const HumaxHeaderAnfang2 = &H78123456 ' die DWORDs 2-9 eines jeden Headers
-Private Const HumaxBasisDatum = #11/17/1858#
+#define HumaxHeaderIntervall    0x8000           // nach diesem Intervall wird der Header wiederholt
+#define HumaxHeaderLaenge       0x4A0            // die Länge eines jeden Headers
+#define HumaxHeaderAnfang       0xFD04417F       // das erste DWORD eines jeden Headers
+#define HumaxHeaderAnfang2      0x78123456       // die DWORDs 2-9 eines jeden Headers
 
-Private Enum THumaxZusInfoIDs
-  HumaxBookmarksID = &H5514
-  HumaxTonSpurenID = &H4823
-End Enum
+typedef enum
+{
+  HumaxBookmarksID = 0x5514,
+  HumaxTonSpurenID = 0x4823
+} THumaxZusInfoIDs;
 
+typedef struct
+{
+  word                  PID;
+  char                  Name[6];                 // meist kürzer, nullterminiert
+} THumaxTonSpur;
 
-Private Type THumaxTonSpur
-  PID As Integer
-  Name As String * 6            ' meist kürzer, nullterminiert
-End Type
+typedef struct
+{
+  dword Anfang;              // dient zur Identifikation des Headers (?)
+  dword Anfang2[8];          // 8 Mal Wiederholung von "V4.x"
+  word VideoPID;
+  word AudioPID;
+  word TeletextPID;
+  byte Leer1[10];            // 10 Bytes leer
+  dword Konstante;           // scheint immer 0x40201 zu sein
+  byte Leer2[8];             // 8 Bytes meist(?) leer
+  dword Unbekannt1;
+  dword Datum;               // Anzahl der Tage seit 17.11.1858 -> Unix = (MJD-40587)*86400
+  dword Zeit;                // Anzahl der Minuten seit 00:00
+  dword Dauer;               // Aufnahmedauer in Sekunden
+  byte Schreibschutz;        // 1 (geschützt) - 0 (nicht geschützt)
+  byte Unbekannt2[15];
+  char Dateiname[32];        // evtl. kürzer, nullterminiert
+} THumaxBlock_allg;
 
-Private Type THumaxBlock_allg
-  Anfang As Long                ' dient zur Identifikation des Headers (?)
-  Anfang2(1 To 8) As Long       ' 8 Mal Wiederholung von "V4.x"
-  VideoPID As Integer
-  AudioPID As Integer
-  TeletextPID As Integer
-  Leer1(1 To 10) As Byte        ' 10 Bytes leer
-  Konstante As Long             ' scheint immer 0x40201 zu sein
-  Leer2(1 To 8) As Byte         ' 8 Bytes meist(?) leer
-  Unbekannt1 As Long
-  Datum As Long                 ' Anzahl der Tage seit 17.11.1858
-  Zeit As Long                  ' Anzahl der Minuten seit 00:00
-  Dauer As Long                 ' Aufnahmedauer in Sekunden
-  Schreibschutz As Byte         ' 1 (geschützt) - 0 (nicht geschützt)
-  Unbekannt2(1 To 15) As Byte
-  Dateiname As String * 32      ' evtl. kürzer, nullterminiert
-End Type
-
-Private Type THumaxBlock_Ende
-  Unbekannt3(1 To 620) As Byte
-  Ende(1 To 30) As Byte         ' vielleicht zur Markierug des Endes (-> eher nicht!)
-End Type
-
-Private Type THumaxBlock_Bookmarks
-  Anzahl As Integer             ' (vermutlich ist die Anzahl ein Long, aber zur Sicherheit...)
-  Leer As Integer
-  Items(1 To 100) As Long
-End Type
-
-Private Type THumaxBlock_Tonspuren
-  Anzahl As Integer
-  Leer As Integer
-  Items(1 To 50) As THumaxTonSpur
-End Type
+typedef struct
+{
+  byte Unbekannt3[620];
+  byte Ende[30];             // vielleicht zur Markierug des Endes (-> eher nicht!)
+} THumaxBlock_Ende;
 
 
-Private Type THumaxHeader
-  Allgemein As THumaxBlock_allg ' allgemeiner Block (Dateiname und Schreibschutz nur im 1. Header aktuell!)
-  ZusInfoID As Integer          ' ID des ZusatzInfo-Blocks
-  ZusInfos(1 To 404) As Byte    ' z.B. 3. Header: Bookmarks, 4. Header: Tonspuren
-  Ende As THumaxBlock_Ende
-End Type
+typedef struct
+{
+  word Anzahl;               // (vermutlich ist die Anzahl ein Long, aber zur Sicherheit...)
+  word Leer;
+  dword Items[100];
+} THumaxBlock_Bookmarks;
 
-Private Type THumaxHeader_Bookmarks
-  Allgemein As THumaxBlock_allg
-  ZusInfoID As Integer          ' wenn Bookmarks vorhanden, ID=0x5514
-  Bookmarks As THumaxBlock_Bookmarks
-  Ende As THumaxBlock_Ende
-End Type
+typedef struct
+{
+  word Anzahl;
+  word Leer;
+  THumaxTonSpur Items[50];
+} THumaxBlock_Tonspuren;
 
-Private Type THumaxHeader_Tonspuren
-  Allgemein As THumaxBlock_allg
-  ZusInfoID As Integer          ' wenn Tonspur-Block, ID=0x4823
-  Tonspuren As THumaxBlock_Tonspuren
-  Ende As THumaxBlock_Ende
-End Type
+
+typedef struct
+{
+  THumaxBlock_allg Allgemein;  // allgemeiner Block (Dateiname und Schreibschutz nur im 1. Header aktuell!)
+  word ZusInfoID;              // ID des ZusatzInfo-Blocks
+  byte ZusInfos[404];          // z.B. 3. Header: Bookmarks, 4. Header: Tonspuren
+  THumaxBlock_Ende Ende;
+} THumaxHeader;
 
 #endif
